9d4eb87d10a5860823a8f4901a6eabcf07aa941d
Gabriel Ulloa <gsulloa@uc.cl>
2017-02-28 20:00:13 -0300

    Agregadas imagenes default y sliders por separado

--- js/index.js
+++ js/index.js
@@ -1,10 +1,23 @@
+const options = {
+	pager:false
+}
+const auspiciosOptions = {
+	ticker: true,
+    speed: 9000,
+    minSlides: 1,
+    maxSlides: 1,
+    tickerHover: true,
+    useCSS: false,
+    slideMargin: 15,
+    slideWidth: 250,
+    slideHeight: 50
+
+}
+
 $(document).ready(function () {
-	$('.main-carrousel').bxSlider({
-  		minSlides: 3,
-  		maxSlides: 3,
-  		slideWidth: 360,
-  		slideMargin: 10,
-  		pager: false
-	});
+	$('.first-carrousel').bxSlider(options);
+	$('.second-carrousel').bxSlider(options);
+	$('.third-carrousel').bxSlider(options);
+	$('ul.auspicios').bxSlider(auspiciosOptions);
 })
 
